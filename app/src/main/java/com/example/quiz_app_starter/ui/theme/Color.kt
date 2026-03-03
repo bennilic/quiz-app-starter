0b6419baad31034a086d1560e8de4da799ce7293
Benjamin Lichtenstein <write.benjamin@icloud.com>
2026-03-03 18:24:33 +0100

    C2: Update the theme and theme colors for light and dark mode.

--- app/src/main/java/com/example/quiz_app_starter/ui/theme/Color.kt
+++ app/src/main/java/com/example/quiz_app_starter/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.quiz_app_starter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFF63B037)
+val Red80 = Color(0xFFEE084F)
+val Black80 = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green40 = Color(0xFFA4DAA1)
+val Red40 = Color(0xFFE3749C)
+val Black40 = Color(0xFF423D3E)
